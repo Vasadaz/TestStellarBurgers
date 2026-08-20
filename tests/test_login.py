a6739eba0dd288abb80eb986cb9fa08c0adee27a
Nikolai Sysoev <vasadaz@ya.ru>
2026-08-20 19:16:15 +0300

    Add test_login.py

---
+++ tests/test_login.py
@@ -0,0 +1,184 @@
+import random
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def test__login__button_login_to_account__true():
+    root_host = 'https://stellarburgers.education-services.ru'
+    user_name = 'Николай'
+    user_email = f"sysoev_53_{random.randint(000000, 999999)}@gmail.com"
+    user_password = "123456"
+
+    driver = webdriver.Chrome()
+    driver.get(root_host)
+
+    # Переход к форме регистрации
+    driver.find_element(By.XPATH, ".//nav/a[@href='/account']").click()  # Кнопка(как ссылка) "Личный кабинет"
+    driver.find_element(By.XPATH, ".//a[text()='Зарегистрироваться']").click()  # Ссылка "Зарегистрироваться"
+
+    # Выполняем регистрацию
+    driver.find_elements(By.NAME, "name")[0].send_keys(user_name)  # Вводим имя
+    driver.find_elements(By.NAME, "name")[1].send_keys(user_email)  # Вводим email
+    driver.find_element(By.NAME, "Пароль").send_keys(user_password)  # Вводим пароль
+    driver.find_element(By.XPATH, ".//button[text()='Зарегистрироваться']").click()  # Кнопка "Зарегистрироваться"
+
+    # Возвращаемся на главную
+    driver.get(root_host)
+    WebDriverWait(driver, 3).until(
+        expected_conditions.element_to_be_clickable((By.XPATH, ".//button[text()='Войти в аккаунт']")))
+
+    driver.find_element(By.XPATH, ".//button[text()='Войти в аккаунт']").click()  # Кнопка(как ссылка) "Войти в аккаунт"
+
+    # Входим в аккаунт
+    WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.XPATH, ".//button[text()='Войти']")))
+    driver.find_element(By.NAME, "name").send_keys(user_email)
+    driver.find_element(By.NAME, "Пароль").send_keys(user_password)
+
+    driver.find_element(By.XPATH, ".//button[text()='Войти']").click()  # Кнопка "Войти"
+
+    # Проверяем результат входа
+    WebDriverWait(driver, 3).until(
+        expected_conditions.element_to_be_clickable((By.XPATH, ".//nav/a[@href='/account']")))
+
+    driver.find_element(By.XPATH, ".//nav/a[@href='/account']").click()  # Кнопка(как ссылка) "Личный кабинет"
+    WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.NAME, "Name")))
+
+    assert '/account' in driver.current_url
+
+    driver.quit()
+
+
+def test__login__button_personal_account__true():
+    root_host = 'https://stellarburgers.education-services.ru'
+    user_name = 'Николай'
+    user_email = f"sysoev_53_{random.randint(000000, 999999)}@gmail.com"
+    user_password = "123456"
+
+    driver = webdriver.Chrome()
+    driver.get(root_host)
+
+    # Переход к форме регистрации
+    driver.find_element(By.XPATH, ".//nav/a[@href='/account']").click() # Кнопка(как ссылка) "Личный кабинет"
+    driver.find_element(By.XPATH, ".//a[text()='Зарегистрироваться']").click() # Ссылка "Зарегистрироваться"
+
+    # Выполняем регистрацию
+    driver.find_elements(By.NAME, "name")[0].send_keys(user_name) # Вводим имя
+    driver.find_elements(By.NAME, "name")[1].send_keys(user_email) # Вводим email
+    driver.find_element(By.NAME, "Пароль").send_keys(user_password) # Вводим пароль
+    driver.find_element(By.XPATH, ".//button[text()='Зарегистрироваться']").click() # Кнопка "Зарегистрироваться"
+
+    # Входим в аккаунт
+    WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.XPATH, ".//button[text()='Войти']")))
+    driver.find_element(By.NAME, "name").send_keys(user_email)
+    driver.find_element(By.NAME, "Пароль").send_keys(user_password)
+    driver.find_element(By.XPATH, ".//button[text()='Войти']").click() # Кнопка "Войти"
+
+    # Проверяем результат входа
+    WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.XPATH, ".//nav/a[@href='/account']")))
+    driver.find_element(By.XPATH, ".//nav/a[@href='/account']").click() # Кнопка(как ссылка) "Личный кабинет"
+    WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.NAME, "Name")))
+
+    assert '/account' in driver.current_url
+
+    driver.quit()
+
+
+def test__login__button_login_for_registration__true():
+    root_host = 'https://stellarburgers.education-services.ru'
+    user_name = 'Николай'
+    user_email = f"sysoev_53_{random.randint(000000, 999999)}@gmail.com"
+    user_password = "123456"
+
+    driver = webdriver.Chrome()
+    driver.get(root_host)
+
+    # Переход к форме регистрации
+    driver.find_element(By.XPATH, ".//nav/a[@href='/account']").click()  # Кнопка(как ссылка) "Личный кабинет"
+    driver.find_element(By.XPATH, ".//a[text()='Зарегистрироваться']").click()  # Ссылка "Зарегистрироваться"
+
+    # Выполняем регистрацию
+    driver.find_elements(By.NAME, "name")[0].send_keys(user_name)  # Вводим имя
+    driver.find_elements(By.NAME, "name")[1].send_keys(user_email)  # Вводим email
+    driver.find_element(By.NAME, "Пароль").send_keys(user_password)  # Вводим пароль
+    driver.find_element(By.XPATH, ".//button[text()='Зарегистрироваться']").click()  # Кнопка "Зарегистрироваться"
+
+    # Переход к форме регистрации
+    WebDriverWait(driver, 3).until(
+        expected_conditions.element_to_be_clickable((By.XPATH, ".//nav/a[@href='/account']")))
+    driver.find_element(By.XPATH, ".//nav/a[@href='/account']").click()  # Кнопка(как ссылка) "Личный кабинет"
+    driver.find_element(By.XPATH, ".//a[text()='Зарегистрироваться']").click()  # Ссылка "Зарегистрироваться"
+
+    # Переходим на страницу входа
+    WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.XPATH, ".//a[text()='Войти']")))
+    driver.find_element(By.XPATH, ".//a[text()='Войти']").click()  # Ссылка "Зарегистрироваться"
+
+    # Входим в аккаунт
+    WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.XPATH, ".//button[text()='Войти']")))
+    driver.find_element(By.NAME, "name").send_keys(user_email)
+    driver.find_element(By.NAME, "Пароль").send_keys(user_password)
+    driver.find_element(By.XPATH, ".//button[text()='Войти']").click()  # Кнопка "Войти"
+
+    # Проверяем результат входа
+    WebDriverWait(driver, 3).until(
+        expected_conditions.element_to_be_clickable((By.XPATH, ".//nav/a[@href='/account']")))
+    driver.find_element(By.XPATH, ".//nav/a[@href='/account']").click()  # Кнопка(как ссылка) "Личный кабинет"
+    WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.NAME, "Name")))
+
+    assert '/account' in driver.current_url
+
+    driver.quit()
+
+
+def test__login__button_login_for_reset_password__true():
+    root_host = 'https://stellarburgers.education-services.ru'
+    user_name = 'Николай'
+    user_email = f"sysoev_53_{random.randint(000000, 999999)}@gmail.com"
+    user_password = "123456"
+
+    driver = webdriver.Chrome()
+    driver.get(root_host)
+
+    # Переход к форме регистрации
+    driver.find_element(By.XPATH, ".//nav/a[@href='/account']").click()  # Кнопка(как ссылка) "Личный кабинет"
+    driver.find_element(By.XPATH, ".//a[text()='Зарегистрироваться']").click()  # Ссылка "Зарегистрироваться"
+
+    # Выполняем регистрацию
+    driver.find_elements(By.NAME, "name")[0].send_keys(user_name)  # Вводим имя
+    driver.find_elements(By.NAME, "name")[1].send_keys(user_email)  # Вводим email
+    driver.find_element(By.NAME, "Пароль").send_keys(user_password)  # Вводим пароль
+    driver.find_element(By.XPATH, ".//button[text()='Зарегистрироваться']").click()  # Кнопка "Зарегистрироваться"
+
+    # Переход к форме регистрации
+    WebDriverWait(driver, 3).until(
+        expected_conditions.element_to_be_clickable((By.XPATH, ".//nav/a[@href='/account']")))
+    driver.find_element(By.XPATH, ".//nav/a[@href='/account']").click()  # Кнопка(как ссылка) "Личный кабинет"
+    driver.find_element(By.XPATH, ".//a[text()='Восстановить пароль']").click()  # Ссылка "Восстановить пароль"
+
+    # Переходим на страницу входа
+    WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.XPATH, ".//a[text()='Войти']")))
+    driver.find_element(By.XPATH, ".//a[text()='Войти']").click()  # Ссылка "Зарегистрироваться"
+
+    # Входим в аккаунт
+    WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.XPATH, ".//button[text()='Войти']")))
+    driver.find_element(By.NAME, "name").send_keys(user_email)
+    driver.find_element(By.NAME, "Пароль").send_keys(user_password)
+    driver.find_element(By.XPATH, ".//button[text()='Войти']").click()  # Кнопка "Войти"
+
+    # Проверяем результат входа
+    WebDriverWait(driver, 3).until(
+        expected_conditions.element_to_be_clickable((By.XPATH, ".//nav/a[@href='/account']")))
+    driver.find_element(By.XPATH, ".//nav/a[@href='/account']").click()  # Кнопка(как ссылка) "Личный кабинет"
+    WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.NAME, "Name")))
+
+    assert '/account' in driver.current_url
+
+    driver.quit()
+
+
+if __name__ == "__main__":
+    test__login__button_login_to_account__true()
+    test__login__button_personal_account__true()
+    test__login__button_login_for_registration__true()
